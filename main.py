982881e470eafe6fbcfaef1e15283e4f0553962b
Dankir30 <kirrdann@gmail.com>
2024-04-17 22:29:23 +0300

    added headers

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ app.add_middleware(
     allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
-    allow_headers=["*"],
+    allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
+                   "Authorization"],
 )
 
 client_session = aiohttp.ClientSession()
